f3c7d221890c47cf477d91ec8c5f58459fd7ecbf
Gurjot <65106574+XXGurjot07@users.noreply.github.com>
2022-01-25 21:44:28 +0530

    Add files via upload

---
+++ mpu6050 with dmp.ino
@@ -0,0 +1,100 @@
+
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps612.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+MPU6050 mpu;
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+float ypr[3];  // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+void setup() {
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+  Serial.begin(115200);
+
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(194);
+  mpu.setYGyroOffset(126);
+  mpu.setZGyroOffset(-65);
+  mpu.setXAccelOffset(-752);
+  mpu.setYAccelOffset(-47);
+  mpu.setZAccelOffset(772);
+
+  if (devStatus == 0) {
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+
+    mpu.setDMPEnabled(true);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  Serial.print(devStatus);
+  Serial.println(F(")"));
+  delay(1000);
+}
+
+void loop() {
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+    Serial.print(ypr[0] * 180 / M_PI);
+    Serial.print("/");
+    Serial.print(ypr[1] * 180 / M_PI);
+    Serial.print("/");
+    Serial.print(ypr[2] * 180 / M_PI);
+
+    Serial.println();
+
+#endif
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_BUILTIN, blinkState);
+  }
+
+
+}
